31d6b435216d59039435e603753d4fa814706bc3
Aidan <115746844+Aidandaman@users.noreply.github.com>
2026-03-06 20:02:25 -0500

    remove unused import

--- src/main/java/frc/robot/subsystems/interfaces/Intake.java
+++ src/main/java/frc/robot/subsystems/interfaces/Intake.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
