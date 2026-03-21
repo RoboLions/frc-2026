4378914d38d12c33cca450392a6d64082ad2ade5
Aidan <115746844+Aidandaman@users.noreply.github.com>
2026-03-21 15:11:12 -0400

    fix comment on swerve

--- src/main/java/frc/robot/subsystems/interfaces/swerve/GeneratedConstants.java
+++ src/main/java/frc/robot/subsystems/interfaces/swerve/GeneratedConstants.java
@@ -56,11 +56,11 @@ public class GeneratedConstants {
     private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
-                // Swerve azimuth does not require much torque output, so we can set a relatively low
-                // stator current limit to help avoid brownouts without impacting performance.
+                // Relatively low stator limit to help with battery performance and no-slip.
                 .withStatorCurrentLimit(Amps.of(70))
                 .withStatorCurrentLimitEnable(true)
-        );    private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
+        );    
+    private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
                 // Swerve azimuth does not require much torque output, so we can set a relatively low
