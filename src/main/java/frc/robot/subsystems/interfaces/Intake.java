fb495b6e568aa43f866e97723cfdfb6efcf1947d
Aidan <115746844+Aidandaman@users.noreply.github.com>
2026-03-24 16:55:37 -0400

    remove unused import, day 2 to NB

--- src/main/java/frc/robot/subsystems/interfaces/Intake.java
+++ src/main/java/frc/robot/subsystems/interfaces/Intake.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.interfaces;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
