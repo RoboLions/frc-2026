81bd1d1ac2cba8893b26100e955d3471c99a1a43
Layth <deepdot123v2@gmail.com>
2026-09-10 19:44:23 -0400

    Deleted all states and made new ones

---
+++ src/main/java/frc/robot/subsystems/statemachines/DriveTrainStateMachine.java
@@ -0,0 +1,8 @@
+public class DriveTrainStateMachine extends StateMachine{
+    public static OutakeState outakeState = new OutakeState();
+    public static BrakeState brakeState = new BrakeState();
+    public static IntakeState intakeState = new IntakeState();
+    public static ShootState shootState = new ShootState();
+    public static IdlekState idleState = new IdleState();
+
+}
---
+++ src/main/java/frc/robot/subsystems/statemachines/scoring/OutakeState.java
@@ -0,0 +1,3 @@
+public class OutakeState extends State {
+    
+}
